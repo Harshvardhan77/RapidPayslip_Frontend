64f9e79b3452d2e751f2ad1155555472c874ea3d
Harshvardhan77 <harshvardhan2024@ipsacademy.org>
2024-06-27 13:43:45 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     proxy: {
       '/api/v1/users': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
     },
   },
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['html2pdf.js'],
+    },
+  },
+});
